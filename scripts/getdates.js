e684bbad84477e801d0a913edba50454af28e9ce
andrewp100 <pintostar911@gmail.com>
2024-01-13 15:53:19 -0700

    Week 1 commit

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+//alert(document.lastModified);//
+let oLastModif = new Date(document.lastModified);
+document.querySelector("#lastModified").textContent = oLastModif;
+
+const options = {
+    year: "numeric"
+};
+document.querySelector("#yearDate").innerHTML = new Date().toLocaleDateString("en-US", options);
+
