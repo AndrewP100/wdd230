10f521589dd9041caccb64e4acb924f2fa04f001
andrewp100 <pintostar911@gmail.com>
2024-02-10 14:09:10 -0700

    Week 5 Commit

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,23 +1,14 @@
 const modeButton = document.querySelector("#mode");
-const header = document.querySelector("header");
 const main = document.querySelector("main");
-const footer = document.querySelector("footer");
-const menu = document.querySelector(".navigation");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🌝")) {
+    if (modeButton.textContent.includes("🕶️")) {
         main.style.background = "#000";
-        header.style.background = "#000";
-        footer.style.background = "#000";
-        menu.style.background = "#000";
-
-        modeButton.textContent = "Light Mode 💡";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
     } else {
-        main.style.background = "#fff";
-        header.style.background = "#333333";
-        footer.style.background = "#333333";
-        menu.style.background = "#333333";
-
-        modeButton.textContent = "Dark Mode 🌝 ";
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
     }
 });
