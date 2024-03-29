8c2aa132b337d110b19098570318c83d0ae505be
andrewp100 <pintostar911@gmail.com>
2024-03-29 12:49:52 -0600

    Week 12 Commit

---
+++ chamber/scripts/redirect.js
@@ -0,0 +1,4 @@
+function redirect() {
+    window.location.href =
+        "https://andrewp100.github.io/wdd230/chamber/join.html";
+}
--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = ``;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=82b9407b7b6d113e077d354c4b29fe74&units=imperial`;
 
 async function apiFetch() {
     try {
